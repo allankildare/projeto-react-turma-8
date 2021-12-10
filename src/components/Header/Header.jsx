e4992a709ebd14043b35f77e4ebc89b8271e2c1b
allankildare <allankildare27@gmail.com>
2021-12-09 21:36:37 -0300

    style: adicao de elementos e estilizacao do header (aula 4)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
-
+import './Header.css'
 export default class Header extends Component {
     render() {
         return (
             <header className="mainHeader">
-                <h1>Bons Drinks</h1>
+                <ul>
+                    <li>Drinks</li>
+                    <li>Sobre nós</li>
+                    <li>
+                        <h1 className="logo">Bons Drinks</h1>
+                    </li>
+                    <li>Nosso time</li>
+                    <li>Contato</li>
+                </ul>
             </header>
         )
     }
