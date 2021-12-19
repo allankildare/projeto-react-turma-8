7cac0116b25de44401a1565b6ee1a5894c4776f5
allankildare <allankildare27@gmail.com>
2021-12-19 15:07:21 -0300

    feat: adicao do componente Button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import style from './Button.module.css'
+
+class Button extends Component {
+    constructor(props) {
+        super(props)
+        this.meuClique = this.meuClique.bind(this)
+        this.state = {
+            disabled: false
+        }
+    }
+    // aqui temos definidos os valores padroes das props desse componente
+    static defaultProps = {
+        type: 'button'
+    }
+
+    meuClique() {
+        this.setState({ disabled: true })
+        setInterval(() => {
+            this.setState({ disabled: false })
+        }, 3000)
+    }
+    
+    render() {
+        console.log(this.props.type)
+        return (
+            <button
+                type={this.props.type}
+                disabled={this.state.disabled ? true : false}
+                onClick={this.meuClique}
+                className={style.btn}
+                >
+                    {this.props.text}
+            </button>
+        )
+    }
+}
+
+export default Button
