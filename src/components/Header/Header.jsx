90ea13476329bf056c2dd3f7952889a234c1b520
allankildare <allankildare27@gmail.com>
2021-12-21 22:36:17 -0300

    feat: menu de navegacao funcional

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import style from './Header.module.css'
+import { Link } from 'react-router-dom'
 export default class Header extends Component {
     render() {
         return (
             <header className={style.mainHeader}>
-                <ul>
-                    <li>Drinks</li>
-                    <li>Sobre nós</li>
-                    <li>
-                        <h1 className="logo">Bons Drinks</h1>
-                    </li>
-                    <li>Nosso time</li>
-                    <li>Contato</li>
-                </ul>
+                <nav>
+                    <Link to="/drinks">Drinks</Link>
+                    <Link to="/sobre-nos">Sobre nós</Link>
+                    <Link to="/">
+                        <h1 className={style.logo}>Bons Drinks</h1>
+                    </Link>
+                    <Link to="/nosso-time">Nosso time</Link>
+                    <Link to="/contato">Contato</Link>
+                </nav>
             </header>
         )
     }
