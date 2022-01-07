cc63b8eb175bf14a0ecf7f97b82f49be19aea18e
allankildare <allankildare27@gmail.com>
2022-01-06 22:09:50 -0300

    chore: nova pagina inicial e rotas aninhadas

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Drinks from './components/Drinks/Drinks'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Center from './components/Center/Center'
 import Logo from './components/Logo/Logo'
+import ExemploState from './components/ExemploState/ExemploState'
+import DrinksPopulares from './components/Drinks/DrinksPopulares'
+import BuscaDrinks from './components/Drinks/BuscaDrinks'
+import MainCover from './components/MainCover/MainCover'
 
 class App extends Component {
   render() {
@@ -16,9 +20,7 @@ class App extends Component {
             <Header />
             <Routes>
               <Route path="/" element={
-                <Center>
-                  <Logo />
-                </Center>
+                <MainCover />
               } />
               <Route path="/sobre-nos" element={
                 <Center>
@@ -28,10 +30,14 @@ class App extends Component {
               <Route path="/nosso-time" element={
                 <Center>
                   <h1>Nosso time</h1>
+                  <ExemploState />
                 </Center>
               } />
               <Route path="/contato" element={<Form />} />
-              <Route path="/drinks" element={<Drinks />} />
+              <Route path="/drinks" element={<Drinks />}>
+                <Route path="populares" element={<DrinksPopulares />} />
+                <Route path="buscar-drinks" element={<BuscaDrinks />} />
+              </Route>
               <Route path="*" element={<h1>Erro 404</h1>} />
             </Routes>
         </Router>
