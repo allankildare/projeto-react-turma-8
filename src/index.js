a0594806923fab443d5900454bb4a9c8177a9a5b
allankildare <allankildare27@gmail.com>
2022-01-11 20:48:46 -0300

    feat: provider do contexto para toda aplicacao

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { UserProvider } from './context'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <UserProvider>
     <App />
-  </React.StrictMode>,
+  </UserProvider>,
   document.getElementById('root')
 )
